39fbf031958ac2a9a496175d0a0b77bf1d1ee43c
Darkjudge1 <Darkjudge1@gmail.com>
2019-07-09 22:42:23 -0700

    Parts 4 and 5 Completed!

--- engine.py
+++ engine.py
@@ -1,38 +1,50 @@
 #Part 0 Completed 6/18/2019
 #Part 1 Completed 6/18/2019.
 #Part 2 Completed 6/26/2019
-#Part 3 in progress.
+#Part 3 Completed 7/1/2019
+#Part 4 Completed 7/8/2019.
+#Part 5
 #Created by John Brunson
 #Tutorial located at: http://rogueliketutorials.com/tutorials/tcod/part-1/
 #TODO: Next version, is it possible to pull these in from a database, JSON, XML, etc.
-import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 import tcod as libtcod
-from entity import Entity
+from entity import Entity, get_blocking_entities_at_location
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
 from render_functions import render_all, clear_all
+from fov_functions import initialize_fov, recompute_fov
+from game_states import GameStates
 
 def main():
+    #TODO: Move these setting out to a setting file in the tutorial doesn't already. Bonus points if they can accept user or randomized input
     screen_width = 80
     screen_height = 50
     map_width = 80
     map_height = 45
+    #rooms in the dungeon
     room_max_size = 10
     room_min_size = 6
     max_rooms = 30
+    # field of view
+    fov_algorithm = 0 #default algorithm for libcod, consider exploring this.
+    fov_light_walls = True
+    fov_radius = 10
+    # monsters
+    max_monsters_per_room = 3
+
+
 
     #tile colors
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
-        'dark_ground': libtcod.Color(50, 50, 150)
+        'dark_ground': libtcod.Color(50, 50, 150),
+        'light_wall': libtcod.Color(30, 110, 50),
+        'light_ground': libtcod.Color(200, 180, 50)
     }
 
     #instantiate a player
-    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
-    #instatiate a npc.
-    npc = Entity(int(screen_width / 2-5), int(screen_height / 2), '@', libtcod.yellow)
-    entities = [npc, player]
+    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True)
+    entities = [player]
    
 
 #input graphics file
@@ -42,18 +54,24 @@ def main():
     con = libtcod.console_new(screen_width, screen_height)
 #game map
     game_map = GameMap(map_width, map_height) 
-    #game_map.make_map()
-    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
-    logging.debug ('Gamemap called from within engine.')
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room)
+    fov_recompute = True
+    fov_map = initialize_fov(game_map)
 # Keyboard and mouse input
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 # Main Game Loop
+    game_state = GameStates.PLAYERS_TURN
 
     while not libtcod.console_is_window_closed():
 
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
-        render_all(con, entities, game_map, screen_width, screen_height, colors)
+
+        if fov_recompute:
+            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
+        
+        render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+        fov_recompute = False
         libtcod.console_flush()
         clear_all(con, entities)
 
@@ -65,10 +83,21 @@ def main():
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
-        if move:
+        if move and game_state == GameStates.PLAYERS_TURN:
             dx, dy = move
-            if not game_map.is_blocked(player.x + dx, player.y +dy):
-                player.move(dx,dy)
+            destination_x = player.x + dx
+            destination_y = player.y + dy
+
+            if not game_map.is_blocked(destination_x, destination_y):
+                target = get_blocking_entities_at_location(entities, destination_x, destination_y)
+                
+                if target:
+                    print('You kick the ' + target.name + ' in the shins, much to its annoyance!')
+                else:
+                    player.move(dx,dy)
+
+                    fov_recompute = True
+                game_state = GameStates.ENEMY_TURN
 
         if exit:
             return True
@@ -76,6 +105,12 @@ def main():
         if fullscreen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
+        if game_state == GameStates.ENEMY_TURN:
+            for entity in entities:
+                if entity != player:
+                    print('The '+  entity.name + ' ponders the meaning of its existence.')
+
+            game_state = GameStates.PLAYERS_TURN 
 
 if __name__ == '__main__':
     main()
--- map_objects/tile.py
+++ map_objects/tile.py
@@ -5,3 +5,5 @@ class Tile:
             block_sight = blocked
 
         self.block_sight = block_sight
+        self.explored = False
+        
